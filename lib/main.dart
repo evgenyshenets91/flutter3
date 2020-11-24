27d1b6db7d2bb2208aff9504af707a318df975d8
Evgeny Shenets <evgenyshenets91@gmail.com>
2020-11-24 23:08:54 +0300

    add padding inside Row

--- lib/main.dart
+++ lib/main.dart
@@ -25,15 +25,23 @@ class MyApp extends StatelessWidget {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Image.asset('images/dice1.png'),
-        ),
-        Expanded(
-          child: Image.asset('images/dice1.png'),
-        )
-      ],
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Image.asset('images/dice1.png'),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
